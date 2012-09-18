d1b858daa7fff49783f765179af62cedc9d12e07
Kirill Agatov <agatovs@gmail.com>
2012-09-18 18:33:10 +0400

    workouts controller changed

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,10 @@
 class WorkoutsController < ApplicationController
+
+  before_filter :find_workout, only: [:show, :edit, :update, :destroy]
+  before_filter :has_access?, only: [:edit, :update, :destroy]
+
   def index
+    @workouts = current_user.workouts
   end
 
   def show
@@ -18,5 +23,18 @@ class WorkoutsController < ApplicationController
   end
 
   def destroy
+    @workout.destroy
+  end
+
+  private
+
+  def find_workout
+    @workout = Workout.find(params[:id])
+  end
+
+  def has_access?
+    unless @workout.user_id == current_user.id
+      raise Exception("!!!")
+    end
   end
 end
