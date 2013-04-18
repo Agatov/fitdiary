c268828865f6e57087326ebc21d5f5f151a1eae6
agatov <agatovs@gmail.com>
2013-04-18 17:47:19 +0400

    rabl initailzer added

---
+++ app/assets/javascripts/refactoring.js
@@ -0,0 +1,10 @@
+//= require lib/jquery-1.9.1
+//= require lib/underscore
+//= require lib/backbone
+//= require lib/marionette
+//= require hamlcoffee
+//= require js-routes
+//= require_tree ./backbone/config
+//= require backbone/app
+//= require_tree ./backbone/entities
+//= require_tree ./backbone/apps
