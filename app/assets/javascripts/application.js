f72527c5b2a74013694f955645542e942650a99a
agatov <agatovs@gmail.com>
2013-04-30 16:27:19 +0400

    end of refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,13 @@
-//= require lib/jquery-1
-// require jquery_ujs
-//= require underscore
-//= require json2
-//= require backbone
-//= require backbone.marionette
+//= require lib/jquery-1.9.1
+//= require lib/underscore
+//= require lib/backbone
+//= require lib/backbone-validation
+//= require lib/marionette
 //= require hamlcoffee
-
-//= require_tree ../templates
-
-//= require initializer
-// require_tree ./app
+//= require lib/laconic
+//= require lib/backbone-ui
+//= require js-routes
+//= require_tree ./backbone/config
+//= require backbone/app
+//= require_tree ./backbone/entities
+//= require_tree ./backbone/apps
